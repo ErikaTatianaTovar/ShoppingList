5ad59bb118ef73eb7e59ea3a0c934b73c7c3496f
Erika Tovar <tatianatovar59@gmail.com>
2023-06-21 12:33:23 -0500

    unifique database

--- ShoppingList/app/src/main/java/com/example/shoppinglist/infraestructure/dblocal/AppDataBase.kt
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/infraestructure/dblocal/AppDataBase.kt
@@ -4,25 +4,26 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.shoppinglist.infraestructure.dblocal.daos.MarketDao
 import com.example.shoppinglist.infraestructure.dblocal.daos.ShoppingDao
 import com.example.shoppinglist.infraestructure.dblocal.entitys.MarketEntity
 import com.example.shoppinglist.infraestructure.dblocal.entitys.ShoppingEntity
 
-@Database(entities = [ShoppingEntity::class,MarketEntity::class], version = 1)
-abstract class ShoppingDataBase : RoomDatabase() {
-
+@Database(entities = [MarketEntity::class, ShoppingEntity::class], version = 3)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun marketDao(): MarketDao
     abstract fun shoppingDao(): ShoppingDao
 
     companion object {
         @Volatile
-        private var INSTANCE: ShoppingDataBase? = null
+        private var INSTANCE: AppDatabase? = null
 
-        fun getInstance(context: Context): ShoppingDataBase {
+        fun getInstance(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
-                    ShoppingDataBase::class.java,
-                    "shopping_database"
+                    AppDatabase::class.java,
+                    "app_database"
                 ).build()
                 INSTANCE = instance
                 instance
